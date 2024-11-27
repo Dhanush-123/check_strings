1b52b81018c65ca67373a37cd781aa7fb3b3b002
DHANUSH <114476777+Dhanush-123@users.noreply.github.com>
2024-11-27 14:48:04 +0530

    Add files via upload

---
+++ check_strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first string: ");
+        string? str1 = Console.ReadLine();
+
+        Console.Write("Enter the second string: ");
+        string? str2 = Console.ReadLine();
+
+      
+        if (str1 == str2)
+        {
+            Console.WriteLine("The strings are equal.");
+        }
+        else
+        {
+            Console.WriteLine("The strings are not equal.");
+        }
+    }
+}
